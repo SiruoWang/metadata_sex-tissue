f6237d8888f42cd6679a678925efcb286cb6d01c
SiruoWang <sara.siruo@outlook.com>
2015-06-04 16:04:58 -0400

    Update sex_query.r

--- sex_query.r
+++ sex_query.r
@@ -1,10 +1,10 @@
 # sex function allows query on cell line and return corresponding sex information. F -> female; M -> male; U-> unknown
-
+library(stringr)
 sex <- function(alist) {
    x <- lapply(alist, function(cell_line) {
   
                     sexdata <- sextable()
-                   
+                    cell_line <- str_trim(cell_line)
                     if (cell_line %in% sexdata$cell_line)
                     {
                       
